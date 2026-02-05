1a1c5685f7d47c8db91bd6a4125bb9e98fc959b1
M87sLaptop <jeanneauhamvandoorn@gmail.com>
2026-02-05 17:07:53 +0100

    Major bug fix
    I've been stupid!
    fixed :
    - leaky assignment of the udp struct
    - amount of data broadcasted (it was WAYYYY too much)

--- plant.c
+++ plant.c
@@ -9,18 +9,14 @@
 #include "lwip/udp.h"
 
 #define UDP_PORT 4444
-#define BEACON_MSG_LEN_MAX 127
+#define BEACON_MSG_LEN_MAX 5
 #define BEACON_TARGET "255.255.255.255"
 
 // define how long in ms the pico waits before measuring & broadcasting again, in this case 15 minutes
 #define SAMPLE_TIME 900000
 
 //the function for broadcasting the data gathered
-void broadcastData(int data) {
-    struct udp_pcb* pcb = udp_new();
-
-    ip_addr_t addr;
-    ipaddr_aton(BEACON_TARGET, &addr);
+void broadcastData(int data, struct udp_pcb* pcb, ip_addr_t addr) {
 
     struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX+1, PBUF_RAM);
     char *req = (char *)p->payload;
@@ -29,10 +25,22 @@ void broadcastData(int data) {
     err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
     pbuf_free(p);
     if (er != ERR_OK) {
-        printf("Failed to send UDP packet! error=%d", er);
+        printf("Failed to send UDP packet! error=%d\n", er);
+        printf("Failed Value : %d\n", data);
     } else {
         printf("Sent packet : %d\n", data);
     }
+    #if PICO_CYW43_ARCH_POLL
+        // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
+        // main loop (not from a timer) to check for Wi-Fi driver or lwIP work that needs to be done.
+        cyw43_arch_poll();
+        sleep_ms(1);
+#else
+        // if you are not using pico_cyw43_arch_poll, then WiFI driver and lwIP work
+        // is done via interrupt in the background. This sleep is just an example of some (blocking)
+        // work you might be doing.
+        sleep_ms(1);
+#endif
 }
 
 //the main loop, gathering data and broadcasting it
@@ -68,6 +76,13 @@ int main()
     adc_gpio_init(MOIST_PIN);
     adc_select_input(1);
 
+    struct udp_pcb* pcb = udp_new();
+
+    ip_addr_t addr;
+    ipaddr_aton(BEACON_TARGET, &addr);
+
+    //udp_set_flags(pcb, UDP_FLAGS_BROADCAST);
+
     // the loop keeping the pico running
     while (true) {
         // reads the data and converts it into a intiger
@@ -75,7 +90,7 @@ int main()
         int data = volts;
 
         // broadcasts the data into the open
-        broadcastData(data);
+        broadcastData(data, pcb, addr);
 
         //let that pico rest
         sleep_ms(SAMPLE_TIME);
